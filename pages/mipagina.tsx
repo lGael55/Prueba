b8d17eb187d422e5311eb878b4a040fd84b4d5d2
Gael Avilez <lgael1406@gmail.com>
2022-11-14 16:09:59 -0600

    Commit inicial

---
+++ pages/mipagina.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Typography, { H2 } from '../src/components/Typography'
+
+export default function mipagina() {
+  return (
+    <Typography>Mi Pagina</Typography>
+    
+  )
+}
