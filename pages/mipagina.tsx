7efe3e0025b854c2c5001b905e078a2d2482cfdf
Gael Avilez <lgael1406@gmail.com>
2022-11-14 16:21:38 -0600

    Respaldo 2

--- pages/mipagina.tsx
+++ pages/mipagina.tsx
@@ -3,7 +3,7 @@ import Typography, { H2 } from '../src/components/Typography'
 
 export default function mipagina() {
   return (
-    <Typography>Mi Pagina</Typography>
+    <Typography>Mi Pagina nueva</Typography>
     
   )
 }
